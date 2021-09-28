8c6b3c362a473895ea71fc46681d3c3d3dfa7fda
Sean <sean.g.jones97@gmail.com>
2021-09-28 22:02:14 +0100

    added heroku config info

--- config/config.js
+++ config/config.js
@@ -15,10 +15,13 @@ module.exports = {
         "dialect": process.env.DB_DIALECT
     },
     "production": {
-        "username": process.env.DB_USER,
-        "password": process.env.DB_PASSWORD,
-        "database": process.env.DB_DATABASE,
-        "host": process.env.HOST,
-        "dialect": process.env.DB_DIALECT
-    }
+        "use_env_variable": "DATABASE_URL",
+        "dialect": "postgres",
+        "dialectOptions": {
+           "ssl": {
+             "require": true,
+             "rejectUnauthorized": false
+           }
+         }
+       }    
 }
