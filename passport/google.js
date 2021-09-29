42d87b91c77f64b1b941fac5241ef79de95b79bd
Sean <sean.g.jones97@gmail.com>
2021-09-29 19:02:07 +0100

    changes to deploy

--- passport/google.js
+++ passport/google.js
@@ -21,7 +21,7 @@ passport.deserializeUser(async (id, done) => {
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3001/api/auth/google/redirect"
+    callbackURL: `${process.env.API_HOST}/api/auth/google/redirect`
     }, async (accessToken, refreshToken, profile, done) => {
         const userService = new UserService();
         const userProviderService = new UserProviderService();
